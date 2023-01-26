36643b4a4d5b53f5687972cb397e2a53073f107c
Jason Park <binarydiver@outlook.com>
2023-01-26 22:15:40 +0900

    rename home page component

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-white text-center text-4xl">Super Board</h1>
+      <button className="bg-blue-500 py-4 px-8 rounded-lg">+</button>
+    </>
+  );
+};
+
+export default Home;
