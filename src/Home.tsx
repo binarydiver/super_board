a08f0c827edaa44c70d3e4330de6014bdddc6436
Jason Park <binarydiver@outlook.com>
2023-01-26 22:42:25 +0900

    add card style

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,6 +4,15 @@ const Home = () => {
   return (
     <>
       <h1 className="text-white text-center text-4xl">Super Board</h1>
+      <div className="grid grid-cols-4 gap-4">
+        <div className="card">
+          <div className="divide-y divide-solid">title</div>
+          <div>url</div>
+        </div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
       <button className="bg-blue-500 py-4 px-8 rounded-lg">+</button>
     </>
   );
